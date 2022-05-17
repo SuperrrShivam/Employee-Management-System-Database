55ab8f6277ea88285310559c62b11a11147a9efd
SuperrrShivam <shivamanand741@gmail.com>
2022-05-17 20:11:19 +0530

    Database of EMS project

---
+++ Employee.sql
@@ -0,0 +1,31 @@
+drop table Employee;
+
+create table Employee(
+ID  NUMBER(10)  NOT NULL  ,       
+BASIC_PAY  FLOAT(126)  ,       
+DA         FLOAT(126),         
+DATE_OF_JOINING    VARCHAR2(255 CHAR) ,
+GROSS    FLOAT(126),       
+HRA   FLOAT(126) ,        
+NAME  VARCHAR2(255 CHAR),
+NET   FLOAT(126),  
+TAX    FLOAT(126),
+primary key (ID)
+                 );
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (55,'Shivam','2022-03-15',50000,20,1600,10000,0,10000);
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (56,'Amarnath','2022-03-15',50000,20,1600,10000,0,10000);
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (57,'Shruthi','2022-03-15',50000,20,1600,10000,0,10000);
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (58,'Vaishnavi','2022-03-15',50000,20,1600,10000,0,10000);
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (59,'Roshini','2022-03-15',50000,20,1600,10000,0,10000);
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (60,'Sachin','2022-03-15',50000,20,1600,10000,0,10000);
+
+insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (61,'Rahul','2022-03-15',50000,20,1600,10000,0,10000);
+
+
+commit;
---
+++ user.sql
@@ -0,0 +1,20 @@
+ drop table "user" ;
+
+create table "user"(
+    userid varchar2(255) not null,
+    address varchar2(255),
+    email varchar2(255),
+    first varchar2(255),
+    last varchar2(255),
+    password varchar2(255),
+    phone number(19),
+    primary key (userid)
+);
+ 
+INSERT INTO "user"(userid,first,last,email,phone,password,address)
+VALUES('100','Shivam','Anand','shivam@gmail.com',878956,'U2FsdGVkX1/lHopzF3zhfA7y8/pwfKzBiCA1YvZm5OE=','abc');
+INSERT INTO "user"(userid,first,last,email,phone,password,address)
+VALUES('101','Sai','Roshini','Sr@gmail.com',878956,'UU2FsdGVkX1/WVNbFe3tyj9W5l5VP1AMzfxY+6YkiwE0=','abc');
+
+
+commit;
